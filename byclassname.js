c14b973a50d05eee5770636d01d52b1e5a7fed1d
ShibinaNoushad <115918289+ShibinaNoushad@users.noreply.github.com>
2022-10-19 14:29:29 +0530

    ByClassName

---
+++ byclassname.js
@@ -0,0 +1,17 @@
+var items=document.getElementsByClassName('list-group-item');
+//console.log(items);
+//console.log(items[1]);
+//items[1].textContent="hello";
+//items[1].style.fontWeight="bold";
+//items[1].style.background="red";
+//items.style.background="red";//wrong way and gives error
+/*for(var i=0;i<items.length;i++)
+{
+    items[i].style.backgroundColor='red';
+}*/
+items[2].style.backgroundColor='green';
+for(var i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight='bold';
+}
+
